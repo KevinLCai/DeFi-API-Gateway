f9e2cbeea052e27dfd57f6a24592455a6d1e40b2
Kevin Cai <kevincai@Kevins-MacBook-Pro.local>
2023-03-18 23:57:42 +0000

    reads candlestick data and formats into JSON for frontend

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
+import csv
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
@@ -19,15 +20,48 @@ def get_data():
     return jsonify({'message': message})
 
 
+# @app.route('/chart', methods=['POST'])
+# def chart():
+#     data = []
+
+#     df = pd.read_csv('daily_BTC.csv')
+#     first_row = df.iloc[0]
+
+#     counter = 0
+#     for index, row in df.iterrows():
+#         new_row = {
+#             'time': row['Date'],
+#             'value': row['Open']
+#         }
+#         # print(new_row)
+#         if counter<4:
+#             print(new_row)
+#             data.append(new_row)
+#             counter += 1
+
+
+#     data.append({'time': '2022-01-05', 'value': 120},)
+
+#     return jsonify(data)
+
+import csv
+
+def format_data(filename):
+    data = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # skip header row
+        for row in reader:
+            date = row[0]
+            close_price = float(row[4])
+            obj = {'time': date, 'value': int(close_price)}
+            data.append(obj)
+    return data
+
+
 @app.route('/chart', methods=['POST'])
 def chart():
-    data = [
-        {'time': '2022-01-01', 'value': 100},
-        {'time': '2022-01-02', 'value': 110},
-        {'time': '2022-01-03', 'value': 105},
-        {'time': '2022-01-04', 'value': 115},
-        {'time': '2022-01-05', 'value': 120},
-    ]
+    data = format_data('daily_BTC.csv')
     return jsonify(data)
 
 if __name__ == '__main__':
