1a4a13e2ebba2eefdf9677c3b83448ca47321955
Kevin Cai <kevincai@Kevins-MBP.home>
2023-03-19 20:26:15 +0000

    Insert to DB with frontend

--- app.py
+++ app.py
@@ -33,24 +33,52 @@ def chart():
     return jsonify(data)
 
 
-# @app.route('/add_token', methods=['POST'])
-# def add_token():
-pw = input("Password: ")
-db = Database(
-    user='root',
-    password=pw,
-    host='localhost',
-    database='defi_trading'
-)
-if db:
-    logging.info("MySQL Database Initialised")
-else:
-    logging.error("MySQL Database Failed to Load")
+# # @app.route('/add_token', methods=['POST'])
+# # def add_token():
+# pw = input("Password: ")
+# db = Database(
+#     user='root',
+#     password=pw,
+#     host='localhost',
+#     database='defi_trading'
+# )
+# if db:
+#     logging.info("MySQL Database Initialised")
+# else:
+#     logging.error("MySQL Database Failed to Load")
 
 
-db.insert_token(21, 'Bitcoin', 'Crypto')
+# db.insert_token(21, 'Bitcoin', 'Crypto')
 
-db.close()
+# db.close()
+
+@app.route('/add_token', methods=['POST'])
+def add_token():
+    data = request.get_json()
+    token_id = data.get('token_id')
+    token_name = data.get('token_name')
+    token_type = data.get('token_type')
+
+    pw = input("Password: ")
+    db = Database(
+        user='root',
+        password=pw,
+        host='localhost',
+        database='defi_trading'
+    )
+
+    if db:
+        logging.info("MySQL Database Initialised")
+    else:
+        logging.error("MySQL Database Failed to Load")
+
+    result = db.insert_token(token_id, token_name, token_type)
+    db.close()
+
+    if result:
+        return jsonify({'status': 'success', 'message': 'Token added successfully'})
+    else:
+        return jsonify({'status': 'error', 'message': 'Failed to add token'})
 
 
 if __name__ == '__main__':
